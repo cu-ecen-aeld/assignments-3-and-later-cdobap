6dd422db953b6746a861ebc3278d15fd34d9faa9
baptiste <reverb.princeton@gmail.com>
2022-08-19 14:06:32 +0000

    assignment2 test

--- examples/autotest-validate/autotest-validate.c
+++ examples/autotest-validate/autotest-validate.c
@@ -27,9 +27,7 @@ bool this_function_returns_false()
  */
 const char *my_username()
 {
-<<<<<<< HEAD
+
     return "cdobap";
-=======
-    return "todo-please-enter-your-username-here-in-my_username";
->>>>>>> assignments-base/assignment2
+
 }
